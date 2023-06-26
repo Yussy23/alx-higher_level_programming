6d56b32cdf0a4778b1b27d0b9c54900d57dd2e59
Yussy23 <yousra.dribi15@gmail.com>
2023-06-26 16:09:44 -0700

    mandatory tasks

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-def safe_print_integer_err(value):
-    import sys
-    try:
-        print("{:d}".format(value))
-    except Exception as i:
-        sys.stderr.write("Exception: {}\n".format(i))
-        return (False)
-    else:
-        return (True)
+def safe_print_list_integers(my_list=[], x=0):
+
+    index = printed_ints = 0
+    while True:
+        try:
+            if index < x:
+                print("{:d}".format(my_list[index]), end='')
+                index += 1
+                printed_ints += 1
+            else:
+                print()
+                return printed_ints
+        except (ValueError, TypeError):
+            index += 1
